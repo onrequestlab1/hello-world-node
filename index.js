6d6e150a77f6f96d412072843aa241850d40cbb4
onrequestlab1 <onrequestlab@gmail.com>
2026-07-07 22:23:43 +0530

    hello

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const revision = 4;
+// Reply with "hello worlds" for any request
+app.use((req, res) => {
+  res.json({ message: "hello worlds", revision: revision });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running ${revision} on port ${PORT}`);
+});
